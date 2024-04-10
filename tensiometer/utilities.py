047a60bc55f5b0c67d8913ac570c9605327cd8b1
mraveri <marco.raveri@unige.it>
2024-04-10 03:12:31 -0700

    Update

--- tensiometer/utilities.py
+++ tensiometer/utilities.py
@@ -243,13 +243,13 @@ def random_samples_reshuffle(chain):
     _reshuffle_indexes = np.arange(len(chain.weights))
     np.random.shuffle(_reshuffle_indexes)
     # filter the chain:
-    if hasattr(chain, 'loglikes'):
+    if hasattr(chain, 'loglikes') and chain.loglikes is not None:
         chain.setSamples(samples=chain.samples[_reshuffle_indexes, :],
                          weights=chain.weights[_reshuffle_indexes],
                          loglikes=chain.loglikes[_reshuffle_indexes])
     else:
         chain.setSamples(samples=chain.samples[_reshuffle_indexes, :],
-                        weights=chain.weights[_reshuffle_indexes],)
+                         weights=chain.weights[_reshuffle_indexes])
     # update:
     chain._weightsChanged()
     chain.updateBaseStatistics()
