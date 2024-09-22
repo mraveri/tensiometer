d0df2f3be3a5c9c78961de7ce27068da2003f9fd
Marco Raveri <mraveri@sas.upenn.edu>
2024-09-22 10:29:32 +0200

    Update

--- tensiometer/tests/test_synthetic_probability.py
+++ tensiometer/tests/test_synthetic_probability.py
@@ -14,8 +14,6 @@ import numpy as np
 from getdist import MCSamples
 
 # tensorflow imports:
-import os
-os.environ['TF_USE_LEGACY_KERAS'] = "1"
 import tensorflow as tf
 import tensorflow_probability as tfp
 
